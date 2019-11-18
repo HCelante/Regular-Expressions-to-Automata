5f1c4cf82fd8d128054b76d80ecc7132690cdfdb
ALISSON DA SILVA VIEIRA <a2046229@18e105-444970>
2019-11-18 16:31:22 -0200

    aAa

--- src/separaExpressoes.py
+++ src/separaExpressoes.py
@@ -1,5 +1,33 @@
+import sys
+
 class Expressoes():
     def math(const):
         constantes = []
-        constantes = const.strip().split()
-        print(constantes)
+        contadorP = 0
+        contadorE = 0
+        contador = 0
+        c = 0
+        a = 0
+        inicio = 0
+        fim = 0
+
+        for i in range(len(const)):
+            constantes.append(const[i])
+        
+        for c in constantes:
+            if (c == '(' or c == ')'):
+                contadorP = contadorP + 1
+
+        if (contadorP > 0):
+            contador = int(contadorP/2)
+            while (a <= contador):
+                for i in constantes:
+                    if (i == '('):
+                        contadorE = contadorE + 1
+                    if (contadorE == contador):
+                        inicio = i
+                        print(inicio)
+                a = contador
+                        
+
+            
